de5d69c8f603aef1736bf59e7d3169971625e2a7
Lenka <aruru.goddes@gmail.com>
2024-04-21 21:24:07 +0300

    Домашка 2.12 Тестирование

---
+++ src/main/java/pro/sky/Homework_2/_Testing/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2._Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Homework_2/_Testing/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2._Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
